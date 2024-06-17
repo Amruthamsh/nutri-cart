7bf5fd0aa1cea2418664706a81e9588efb73bf8f
Amruthamsh A <amruthaamsh@gmail.com>
2024-06-17 15:51:32 +0530

    packed foods

--- app/(tabs)/Packed.jsx
+++ app/(tabs)/Packed.jsx
@@ -1,14 +1,155 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { useState } from "react";
+import {
+  Button,
+  Image,
+  View,
+  StyleSheet,
+  Text,
+  ScrollView,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
 
-export class Packed extends Component {
-  render() {
-    return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
-    )
-  }
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const genAI = new GoogleGenerativeAI(process.env.EXPO_PUBLIC_GEMINI_API_KEY);
+export default function Packed() {
+  const [imageURI, setImage] = useState(null);
+  const [imageURI2, setImage2] = useState(null);
+  const [nutritionData, setNutrtionData] = useState(null);
+
+  const generateResult = async () => {
+    try {
+      // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
+      const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+      const base64ImageData1 = await FileSystem.readAsStringAsync(imageURI, {
+        encoding: FileSystem.EncodingType.Base64,
+      });
+
+      const base64ImageData2 = await FileSystem.readAsStringAsync(imageURI2, {
+        encoding: FileSystem.EncodingType.Base64,
+      });
+
+      // Combine image data into an array
+      const images = [
+        {
+          inlineData: {
+            data: base64ImageData1,
+            mimeType: "image/png",
+          },
+        },
+        {
+          inlineData: {
+            data: base64ImageData2,
+            mimeType: "image/png",
+          },
+        },
+      ];
+
+      const prompt = `Give nutrition of the food in the first image (front) considering information from the second image (back).`; // Adjust prompt as needed
+
+      const result = await model.generateContent([prompt, ...images]);
+
+      const response = await result.response;
+      const text = response.text();
+      setNutrtionData(text);
+      console.log(text);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+
+      quality: 1,
+      selectionLimit: 1,
+    });
+
+    if (!imageURI) setImage(result.assets[0].uri);
+    else setImage2(result.assets[0].uri);
+  };
+
+  const camImage = async () => {
+    try {
+      const permissionResult =
+        await ImagePicker.requestCameraPermissionsAsync();
+
+      if (permissionResult.granted === false) {
+        alert("You've refused to allow this appp to access your camera!");
+        return;
+      }
+      let result = await ImagePicker.launchCameraAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        quality: 0.6,
+        cameraType: "back",
+        selectionLimit: 2,
+      });
+
+      if (!result.canceled) {
+        if (!imageURI) setImage(result.assets[0].uri);
+        else setImage2(result.assets[0].uri);
+      }
+    } catch (error) {
+      console.error("Error picking image: ", error);
+    }
+  };
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <View style={styles.buttons}>
+          <Button
+            title="Use Camera to click a Food pic"
+            onPress={camImage}
+            style={styles.button}
+            color="#34C759"
+          />
+          <Button
+            title="Pick an image from camera roll"
+            onPress={pickImage}
+            style={styles.button}
+            color="#34C759"
+          />
+        </View>
+        {imageURI && <Image source={{ uri: imageURI }} style={styles.image} />}
+        {imageURI2 && (
+          <Image source={{ uri: imageURI2 }} style={styles.image} />
+        )}
+        {imageURI && imageURI2 && (
+          <Button title="Generate Data" onPress={generateResult} />
+        )}
+
+        {nutritionData && (
+          <Text style={styles.generatedText}>{nutritionData}</Text>
+        )}
+      </ScrollView>
+    </View>
+  );
 }
 
-export default Packed
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 12,
+    backgroundColor: "#000",
+  },
+  buttons: {
+    marginTop: 10,
+    gap: 10,
+    flexDirection: "column", // Add this to make the buttons inline
+  },
+  button: {},
+  image: {
+    marginVertical: 10,
+    width: 240,
+    height: 180,
+  },
+  generatedText: {
+    color: "white",
+  },
+});
--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,45 +1,24 @@
 import React from "react";
-import { View, Image, Text, TouchableOpacity, StyleSheet } from "react-native";
+import {
+  View,
+  Image,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+} from "react-native";
+import { images } from "../../constants";
 
 const Hero = () => {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <Image
-          source={{
-            uri: "https://i.pinimg.com/564x/8f/8c/7c/8f8c7c663e10aba3553a5d43a245796a.jpg",
-          }}
+          source={images.HomePage}
           style={styles.image}
-          resizeMode="cover"
+          resizeMode="contain"
           borderRadius={10}
         />
-      </View>
-      <View style={styles.textContainer}>
-        <Text style={styles.title}>
-          Before they sold out
-          {"\n"}
-          readymade gluten
-        </Text>
-        <Text style={styles.paragraph}>
-          Copper mug try-hard pitchfork pour-over freegan heirloom neutra air
-          plant cold-pressed tacos poke beard tote bag. Heirloom echo park
-          mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon
-          try-hard chambray.
-        </Text>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => console.log("Button 1 pressed")}
-          >
-            <Text style={styles.buttonText}>Button</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => console.log("Button 2 pressed")}
-          >
-            <Text style={styles.buttonText}>Button</Text>
-          </TouchableOpacity>
-        </View>
       </View>
     </View>
   );
@@ -57,41 +36,8 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   image: {
-    width: 300,
-    height: 200,
+    width: Dimensions.get("screen").width - 16,
     borderRadius: 10,
-  },
-  textContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    paddingHorizontal: 20,
-  },
-  title: {
-    fontSize: 24,
-    color: "#fff",
-    fontWeight: "bold",
-    marginBottom: 10,
-  },
-  paragraph: {
-    fontSize: 16,
-    color: "#ccc",
-    marginBottom: 20,
-  },
-  buttonContainer: {
-    flexDirection: "row",
-    justifyContent: "pace-between",
-    alignItems: "center",
-  },
-  button: {
-    backgroundColor: "#4B51EF",
-    padding: 10,
-    borderRadius: 10,
-    marginRight: 10,
-  },
-  buttonText: {
-    fontSize: 16,
-    color: "#fff",
   },
 });
 
